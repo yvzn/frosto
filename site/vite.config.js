efefef083c45e9f39d26cf9094069aa51087c4a1
Yvan Razafindramanana <mail@ludeo.net>
2021-12-31 06:42:46 +0100

    feat(front): add sign-up page

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,11 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 const config = defineConfig({
 	build: {
 		outDir: '../docs',
 		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				'sign-up': resolve(__dirname, 'sign-up.html')
+			}
+		}
 	},
-	base: ""
+	base: ''
 });
 
 export default config;
