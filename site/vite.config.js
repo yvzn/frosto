1c14313b64812c88a5bfcba75fc25c9c8304b90e
Yvan Razafindramanana <mail@ludeo.net>
2022-09-14 11:02:14 +0000

    Fix API URL in build
    use external package to resolve dynamically

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,18 +1,24 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dotEnvHTMLPlugin from 'vite-plugin-dotenv-in-html';
 
-const config = defineConfig({
-	build: {
-		emptyOutDir: true,
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html'),
-				'sign-up': resolve(__dirname, 'sign-up.html'),
-				'sign-up-complete': resolve(__dirname, 'sign-up-complete.html')
+const config = defineConfig(({ mode }) => {
+	return {
+		build: {
+			emptyOutDir: true,
+			rollupOptions: {
+				input: {
+					main: resolve(__dirname, 'index.html'),
+					'sign-up': resolve(__dirname, 'sign-up.html'),
+					'sign-up-complete': resolve(__dirname, 'sign-up-complete.html')
+				}
 			}
-		}
-	},
-	base: ''
+		},
+		base: '',
+		plugins: [
+			dotEnvHTMLPlugin(mode),
+		]
+	};
 });
 
 export default config;
