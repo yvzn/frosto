00b36268af4e0888e2688b273588b2943057d606
Yvan Razafindramanana <mail@ludeo.net>
2022-10-14 04:19:31 +0000

    chore(api): add TypeScript

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-		es2021: true,
-	},
-	extends: "eslint:recommended",
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: "latest",
-	},
-	rules: {},
-};
-
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
