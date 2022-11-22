0bd46bcedf5239fa0e37cc4c722d4f29665e454c
yvzn <mail@ludeo.net>
2022-11-22 07:25:18 +0100

    fix(app): wording for SEO

--- app/src/components/TheWelcome.vue
+++ app/src/components/TheWelcome.vue
@@ -2,24 +2,17 @@
 	<section class="container py-5">
 		<div class="row flex-lg-row-reverse align-items-center py-lg-5">
 			<article class="col-10 col-sm-8 col-lg-6 p-4 p-md-5">
-				<img
-					src="/undraw_weather_re_qsmd.svg"
-					class="d-block mx-lg-auto img-fluid"
-					alt="Illustration d'une personne observant des icônes de changement de météo"
-					width="600"
-					height="278"
-				/>
+				<img src="/undraw_weather_re_qsmd.svg" class="d-block mx-lg-auto img-fluid"
+					alt="Une personne observe des icônes de changement de météo" width="600" height="278" />
 			</article>
 			<article class="col-lg-6 p-4 p-md-5">
 				<h1 class="fw-light">Anticipez les gelées</h1>
 				<p class="lead text-muted">
-					Recevez une alerte lorsque la météo prévoit des températures négatives
+					Recevez gratuitement une alerte lorsque la météo prévoit des températures négatives
 					dans votre ville.
 				</p>
 				<p>
-					<RouterLink class="btn btn-primary my-2" to="/search"
-						>Commencer</RouterLink
-					>
+					<RouterLink class="btn btn-primary my-2" to="/search">Commencer</RouterLink>
 				</p>
 			</article>
 		</div>
@@ -55,21 +48,12 @@
 	</section>
 	<footer class="text-muted py-5">
 		<div class="container">
-			<p>
-				Les données météo sont fournies par <em>Microsoft</em> et
-				<em>AccuWeather</em>. Nous nous efforçons de protéger au mieux les
+			<p>Les données météo sont fournies par <em>AccuWeather</em>. Nous nous efforçons de protéger au mieux les
 				données personnelles que vous nous confiez.
 			</p>
-			<p>
-				Service
-				<a href="https://github.com/yvzn/frosto/" target="_blank" rel="noopener"
-					>open source</a
-				>
-				maintenu par des
-				<a href="mailto:team-frosto[at]outlook.fr">bénévoles</a>. Illustrations
-				par
-				<a href="https://undraw.co/" target="_blank" rel="noopener">unDraw</a>.
-			</p>
+			<p>Service <a href="https://github.com/yvzn/frosto/" target="_blank" rel="noopener">open source</a> maintenu
+				par des bénévoles. Illustrations par <a href="https://undraw.co/" target="_blank"
+					rel="noopener">unDraw</a>.</p>
 		</div>
 	</footer>
 </template>
