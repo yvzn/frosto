45c2b59c3351a548e2f224b06431e93a5a2ce4fa
Yvan Razafindramanana <mail@ludeo.net>
2025-08-29 09:41:45 +0200

    feat(api): multilingual sign-up completion URL

--- api/src/Data/AppSettings.cs
+++ api/src/Data/AppSettings.cs
@@ -6,4 +6,7 @@ internal class AppSettings
 {
 	private static string siteUrl = Environment.GetEnvironmentVariable("SITE_URL") ?? throw new Exception("SITE_URL variable not set");
 	public static string SiteUrl => siteUrl;
+
+	private static string siteEnUrl => Environment.GetEnvironmentVariable("SITE_EN_URL") ?? throw new Exception("SITE_EN_URL variable not set");
+	public static string SiteEnUrl => siteEnUrl;
 }
