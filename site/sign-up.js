3f492eb84e3aa3987e437544a312ab9ba57b4c2e
Yvan Razafindramanana <mail@ludeo.net>
2025-11-20 09:04:36 +0100

    Reapply "feat(site): enhance loading feedback and error handling during form submission"
    This reverts commit 0762cc9c77dfed941411dbbbec0a43db6040d837.

--- site/sign-up.js
+++ site/sign-up.js
@@ -7,15 +7,56 @@ if (import.meta.env.MODE === "development") {
 	signupForm.action = import.meta.env.VITE_SIGNUP_URL;
 }
 
-signupForm.addEventListener('submit', onFormSubmit);
+signupForm.addEventListener('submit', showLoadingText);
 
-function onFormSubmit(event) {
+function showLoadingText(event) {
 	if (formStatus == 'IDLE') {
 		var button = document.querySelector('button');
 		button.innerHTML = '<span class="spinner-border spinner-border-sm fs-6" role="status" aria-hidden="true"></span> Veuillez patienter...';
 		formStatus = 'PENDING';
+
+		updateLoadingText.timeoutId = setTimeout(updateLoadingText, 10_000);
+
+		// trick to allow setTimeout during form submission
+		event.preventDefault();
+		signupForm.submit();
 	} else {
 		event.preventDefault();
+	}
+}
+
+function updateLoadingText() {
+	var button = document.querySelector('button');
+	button.innerHTML = '<span class="spinner-border spinner-border-sm fs-6" role="status" aria-hidden="true"></span> Cela prend plus de temps que prévu...';
+
+	showFailureText.timeoutId = setTimeout(showFailureText, 50_000);
+}
+
+function showFailureText() {
+	var button = document.querySelector('button');
+
+	var failureMessage = document.createElement('div');
+	failureMessage.className = 'alert alert-danger mt-3';
+	failureMessage.role = 'alert';
+	failureMessage.innerText = "Un problème est survenu lors de l'inscription.";
+
+	button.insertAdjacentElement('beforebegin', failureMessage);
+
+	button.innerHTML = 'Réessayer en actualisant la page';
+	button.addEventListener('click', function (event) {
+		event.preventDefault();
+		location.reload();
+	});
+}
+
+addEventListener('beforeunload', clearLoadingTimeouts);
+
+function clearLoadingTimeouts() {
+	if (updateLoadingText.timeoutId) {
+		clearTimeout(updateLoadingText.timeoutId);
+	}
+	if (showFailureText.timeoutId) {
+		clearTimeout(showFailureText.timeoutId);
 	}
 }
 
@@ -30,12 +71,12 @@ function healthCheck(retries) {
 	var url = import.meta.env.VITE_HEALTHCHECK_URL;
 
 	request.timeout = 30 * 1000;
-	request.ontimeout = function() {
+	request.ontimeout = function () {
 		if (retries > 1) {
 			healthCheck(retries - 1);
 		}
 	};
-	request.onerror = function() {
+	request.onerror = function () {
 		if (retries > 1) {
 			healthCheck(retries - 1);
 		}
