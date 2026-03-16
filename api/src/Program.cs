4bb7ebd18bd6335f9cdfd8bde74d507c001424f3
Yvan Razafindramanana <mail@ludeo.net>
2026-03-16 09:57:13 +0100

    feat(api): add weather forecast endpoint

--- api/src/Program.cs
+++ api/src/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using Azure.Data.Tables;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,12 +24,18 @@ var host = new HostBuilder()
 				.WithName($"{tableName}TableClient");
 			}
 		});
+
+		services.AddHttpClient("default", client =>
+		{
+			client.DefaultRequestHeaders.UserAgent.ParseAdd(
+				$"Frosto/{Assembly.GetExecutingAssembly().GetName().Version} (https://github.com/yvzn/frosto)");
+		});
 	})
 	.ConfigureLogging(logging =>
-    {
+	{
 		logging.SetMinimumLevel(LogLevel.Warning);
 		logging.AddFilter("Function", LogLevel.Warning);
-    })
+	})
 	.Build();
 
 host.Run();
--- weather-forecast/Forecast.cs
+++ weather-forecast/Forecast.cs
@@ -2,5 +2,5 @@ namespace weather;
 
 public record Forecast(DateOnly Date, decimal Minimum, decimal Maximum)
 {
-	internal static readonly decimal defaultTemperatureThreshold = 1.0m;
+	public static readonly decimal defaultTemperatureThreshold = 1.0m;
 }
